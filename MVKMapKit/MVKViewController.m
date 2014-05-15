c502e1e10210acd6a51905afa4276660481f3c7f
Justin R. Miller <incanus@codesorcery.net>
2014-05-15 12:36:52 -0700

    nicer demo tweaks

--- MVKMapKit/MVKViewController.m
+++ MVKMapKit/MVKViewController.m
@@ -18,10 +18,11 @@
 
     self.view.backgroundColor = [UIColor orangeColor];
 
-    MVKMapView *mapView = [[MVKMapView alloc] initWithFrame:CGRectMake(50, 50, 512, 512)];
+    MVKMapView *mapView = [[MVKMapView alloc] initWithFrame:CGRectInset(self.view.bounds, 50, 50)];
+    mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [self.view addSubview:mapView];
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^(void)
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^(void)
     {
         [mapView setCenterCoordinate:CLLocationCoordinate2DMake(45, -122) zoomLevel:6 animated:YES];
     });
