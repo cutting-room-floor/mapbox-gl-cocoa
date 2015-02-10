d3e028eb30f76ad32e80e10345809611612b88c6
Justin R. Miller <incanus@codesorcery.net>
2015-02-10 11:28:15 -0800

    fixes #83: pack up shop & point people to gl-native

--- remove_docs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo
-echo "Removing docs from ~/Library/Developer/Shared/Documentation/DocSets..."
-echo
-
-rm -rfv ~/Library/Developer/Shared/Documentation/DocSets/com.mapbox.Mapbox-GL-*
--- test/MGLTAppDelegate.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface MGLTAppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-@end
